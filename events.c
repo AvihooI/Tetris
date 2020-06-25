726a91b305336b900e43385406fe1bc5698ccb66
Avihoo Ilan <AvihooI@Gmail.com>
2020-06-25 15:19:45 +0300

    game controls no longer use window events which allows for much smoother and consistent play

--- events.c
+++ events.c
@@ -5,7 +5,11 @@
 #include <SDL.h>
 #include "events.h"
 
+
+
 unsigned int baseEventsSDLTick;
+unsigned int baseKeyboardStateSDLTick;
+
 
 void handleGameInput(SDL_Event *e);
 
@@ -13,9 +17,21 @@ void handleTicks();
 
 void handleMenuInput(SDL_Event *e);
 
+void handleGameInputKeyboardState();
+
+void doRawInput();
+
 void initEvents()
 {
 	baseEventsSDLTick = SDL_GetTicks();
+	baseKeyboardStateSDLTick = SDL_GetTicks();
+
+	keyboardCooldowns.downCooldown = 0;
+	keyboardCooldowns.leftCooldown = 0;
+	keyboardCooldowns.rightCooldown = 0;
+	keyboardCooldowns.rotateClockwiseCooldown = 0;
+	keyboardCooldowns.rotateCounterClockwiseCooldown = 0;
+	keyboardCooldowns.dropCooldown = 0;
 }
 
 int doEvents()
@@ -45,6 +61,7 @@ void handleMenuInput(SDL_Event *e)
 {
 	if ((*e).type == SDL_KEYDOWN)
 	{
+
 		switch ((*e).key.keysym.scancode)
 		{
 			case SDL_SCANCODE_RETURN2:
@@ -70,18 +87,6 @@ void handleMenuInput(SDL_Event *e)
 	}
 }
 
-void handleTicks()
-{
-	unsigned int currentSDLTick = SDL_GetTicks();
-
-	if (currentSDLTick - baseEventsSDLTick >= MILLISECONDS_PER_TICK)
-	{
-		baseEventsSDLTick = currentSDLTick;
-
-		doAction(TICK);
-	}
-}
-
 void handleGameInput(SDL_Event *e)
 {
 	if ((*e).type == SDL_KEYDOWN)
@@ -92,29 +97,95 @@ void handleGameInput(SDL_Event *e)
 			case SDL_SCANCODE_RETURN:
 				pause_unpause();
 				break;
-			case SDL_SCANCODE_LEFT:
-				doAction(MOVE_LEFT);
-				break;
-			case SDL_SCANCODE_RIGHT:
-				doAction(MOVE_RIGHT);
-				break;
-			case SDL_SCANCODE_UP:
-				doAction(ROTATE_CLOCKWISE);
-				break;
-			case SDL_SCANCODE_LCTRL:
-				doAction(ROTATE_COUNTER_CLOCKWISE);
-				break;
-			case SDL_SCANCODE_DOWN:
-				doAction(MOVE_DOWN);
-				break;
 			case SDL_SCANCODE_ESCAPE:
 				menuState.isActive = 1;
 				break;
+		}
+	}
+
+	else if ((*e).type == SDL_KEYUP)
+	{
+		switch((*e).key.keysym.scancode)
+		{
+			case SDL_SCANCODE_UP:
+				keyboardCooldowns.rotateClockwiseCooldown = 0;
+				break;
+			case SDL_SCANCODE_LCTRL:
+				keyboardCooldowns.rotateCounterClockwiseCooldown = 0;
+				break;
+			case SDL_SCANCODE_DOWN:
+				keyboardCooldowns.downCooldown = 0;
+				break;
+			case SDL_SCANCODE_LEFT:
+				keyboardCooldowns.leftCooldown = 0;
+				break;
+			case SDL_SCANCODE_RIGHT:
+				keyboardCooldowns.rightCooldown = 0;
+				break;
 			case SDL_SCANCODE_SPACE:
-				doAction(DROP);
+				keyboardCooldowns.dropCooldown = 0;
 				break;
 
 		}
 	}
 }
 
+void handleTicks()
+{
+	unsigned int currentSDLTick = SDL_GetTicks();
+
+	if (currentSDLTick - baseEventsSDLTick >= MILLISECONDS_PER_TICK)
+	{
+		baseEventsSDLTick = currentSDLTick;
+
+		doAction(TICK);
+	}
+
+	if (currentSDLTick - baseKeyboardStateSDLTick >= MILLISECONDS_PER_KEYBOARD_CHECK)
+	{
+		baseKeyboardStateSDLTick = currentSDLTick;
+
+		doRawInput();
+	}
+}
+
+unsigned int rawInputProcess(unsigned const char* keyboardState, unsigned char scanCode, int *currentCooldown, int maxCooldown)
+{
+	if (keyboardState[scanCode])
+	{
+		if (*currentCooldown == 0)
+		{
+			*currentCooldown = maxCooldown;
+			return 1;
+		}
+		else if (*currentCooldown > 0)
+		{
+			(*currentCooldown)--;
+		}
+	}
+
+	return 0;
+}
+
+void doRawInput()
+{
+	unsigned const char* keyboardState = SDL_GetKeyboardState(NULL);
+
+	if (rawInputProcess(keyboardState, SDL_SCANCODE_LEFT, &keyboardCooldowns.leftCooldown, MAX_MOVEMENT_KEYBOARD_COOLDOWN))
+		doAction(MOVE_LEFT);
+
+	if (rawInputProcess(keyboardState, SDL_SCANCODE_RIGHT, &keyboardCooldowns.rightCooldown, MAX_MOVEMENT_KEYBOARD_COOLDOWN))
+		doAction(MOVE_RIGHT);
+
+	if (rawInputProcess(keyboardState, SDL_SCANCODE_DOWN, &keyboardCooldowns.downCooldown, MAX_MOVEMENT_KEYBOARD_COOLDOWN))
+		doAction(MOVE_DOWN);
+
+	if (rawInputProcess(keyboardState, SDL_SCANCODE_SPACE, &keyboardCooldowns.dropCooldown, MAX_DROP_KEYBOARD_COOLDOWN))
+		doAction(DROP);
+
+	if (rawInputProcess(keyboardState, SDL_SCANCODE_UP, &keyboardCooldowns.rotateClockwiseCooldown, MAX_ROTATION_KEYBOARD_COOLDOWN))
+		doAction(ROTATE_CLOCKWISE);
+
+	if (rawInputProcess(keyboardState, SDL_SCANCODE_LCTRL, &keyboardCooldowns.rotateCounterClockwiseCooldown, MAX_ROTATION_KEYBOARD_COOLDOWN))
+		doAction(ROTATE_COUNTER_CLOCKWISE);
+}
--- events.h
+++ events.h
@@ -9,6 +9,22 @@
 #include "logic.h"
 #include "menu.h"
 
+#define MAX_MOVEMENT_KEYBOARD_COOLDOWN 85
+#define MAX_ROTATION_KEYBOARD_COOLDOWN -1
+#define MAX_DROP_KEYBOARD_COOLDOWN -1
+
+struct
+{
+	signed int leftCooldown;
+	signed int rightCooldown;
+	signed int downCooldown;
+	signed int dropCooldown;
+	signed int rotateClockwiseCooldown;
+	signed int rotateCounterClockwiseCooldown;
+} keyboardCooldowns;
+
+#define MILLISECONDS_PER_KEYBOARD_CHECK 1
+
 void initEvents();
 
 int doEvents();
